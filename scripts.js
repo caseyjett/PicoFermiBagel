9af3a01e5a0c03839ab8b7789e52ea4979c3d163
Casey Jett <crjett01@gmail.com>
2020-05-29 00:41:12 -0500

    Fix if statement ordering, add some pico

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
+//No duplicates as of now
 
+// // Note: typeof will reveal randomNum elements as strings and not numbers. 
+// //If for some reason as I work this code, I need the types in my array to be numbers, use following code
+// //let changeType = randomNum.map(Number); 
+// // console.log(typeof changeType[0]); 
 
 // //This will generate the random number for the game. 
 function getRandom(min, max) {
@@ -6,12 +11,12 @@ function getRandom(min, max) {
 
 }
 
-//Obviously these will need to go back to (0, 9); 
-const randomNum1 = getRandom(1, 1).toString(); //Min should always be 1 for the first digit
-const randomNum2 = getRandom(0, 1).toString();
-const randomNum3 = getRandom(0, 1).toString();
+//(1, 999) -- when testing with 0/1s remove excess random numbers and use one string.  
+const randomNum1 = getRandom(1, 3).toString(); 
+const randomNum2 = getRandom(0, 3).toString();
+const randomNum3 = getRandom(0, 3).toString();
 
-let randomArr = [randomNum1, randomNum2, randomNum3]; 
+let randomArr = [randomNum1, randomNum2, randomNum3]; //this will become randomNum.toString().split("")
 console.log(randomArr); 
 
 let num1 = document.getElementById('num1');
@@ -27,6 +32,20 @@ function totalFermi(a, b) {
     }
     return true;
 }
+
+function pico(a, b) {
+    let pico = []; 
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === b[i -1] || a[i] === b[i - 2] || a[i] === b[i+1] || a[i] === b[i + 2]) {
+            pico.push([i]);
+        }
+    } return pico.length
+}
+
+let cat = [2, 4, 7]; 
+let dog = [4, 7, 2]; 
+
+console.log(pico(cat, dog))
 
 function fermi(a, b) {
       let fermi = []; 
@@ -48,8 +67,6 @@ guess.addEventListener('click', () => {
     let num1r = num1.value;
     let num2r = num2.value; 
     let num3r = num3.value; 
-    let nums = num1r + num2r + num3r; 
-    
     let newNum = num1r + num2r + num3r; 
     let arr = newNum.split(""); 
     console.log(arr); 
@@ -57,20 +74,28 @@ guess.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
  
+
+
+    if (!totalFermi(arr, randomArr)) {
+        li.textContent = newNum + ` - BAGEL`; 
+        ul.appendChild(li);  
+    } 
+
+    if (fermi(arr, randomArr) > 0) {
+        let numOfFermi = fermi(arr, randomArr);
+        li.textContent = newNum + ` -  ${numOfFermi} Fermi`; 
+        ul.appendChild(li);
+    } 
+
+    if (pico(arr, randomArr) > 0) {
+        let numOfPico = pico(arr, randomArr); 
+        li.textContent = newNum + ` -  ${numOfPico} Pico`; 
+        ul.appendChild(li);
+    }
+
     if (totalFermi(arr, randomArr)) {
         li.textContent = `Total Fermi - YOU WIN!`; 
         ul.appendChild(li);  
-    } 
-
-    if (!totalFermi(arr, randomArr)) {
-        li.textContent = nums + ` - BAGEL`; 
-        ul.appendChild(li);  
-    } 
-
-    if (fermi(arr, randomArr) > 0 && fermi(arr, randomArr) < 3) {
-        let numOfFermi = fermi(arr, randomArr);
-        li.textContent = nums + ` -  ${numOfFermi} Fermi`; 
-        ul.appendChild(li);
     } 
     
     num1.value = ""; 
@@ -81,10 +106,17 @@ guess.addEventListener('click', () => {
  giveUp.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
-    li.textContent = `${randomArr.join("")} - Never give up!`; //instead of console.logging this add it to the document
+    li.textContent = `${randomArr.join("")} - Never give up!`; 
     ul.appendChild(li); 
 })
+
+
 
 //Ideas for input: 
 //https://stackoverflow.com/questions/42067911/input-field-restrict-to-one-digit
 //https://www.w3schools.com/js/js_validation.asp
+//.insertBefore to fix list order
+
+
+// let arr = [1, 4, 6, 7]; 
+// console.log(`I have ${arr.length} numbers. They are ${arr}`)
